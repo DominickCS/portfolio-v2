03ca1d7e0cd51769017b9e6e1bd9a273f678a7b4
Dominick Smith <dominick.smith938@gmail.com>
2025-02-27 13:59:26 -0600

    style update

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <div className="text-3xl text-center">
-        <h1 className="text-3xl text-center intro">Hi, I’m Dominick</h1>
-        <p className="handEmoji">👋</p>
+      <div className="introContainer text-3xl text-center intro">
+        <h1 className="text-3xl text-center pr-2">Hi, I’m Dominick</h1>
+        <p className="handEmoji pl-2">👋</p>
       </div>
       <Image
         src={`/pfp.jpg`}
@@ -15,7 +15,7 @@ export default function Home() {
         className="pfp flex justify-center mx-auto py-8"
       ></Image>
       <h2 className="text-2xl text-center caption">
-        I Build Modern & Scalable Web Experiences
+        I craft sleek, scalable, and high-performance web experiences.
       </h2>
     </main>
   );
