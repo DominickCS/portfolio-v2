3df1232c96583f2a219c6b5cda85c287d797052c
Dominick Smith <dominick.smith938@gmail.com>
2025-02-27 14:28:13 -0600

    new nav link

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,15 +3,18 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center pb-4">
       <Link className="link" href={`/`}>
         Home
       </Link>
-      <Link className="link" href={`/aboutMe`}>
+      <Link className="link" href={`/aboutme`}>
         About Me
       </Link>
       <Link className="link" href={`/projects`}>
         Projects
+      </Link>
+      <Link className="link" href={`/contact`}>
+        Contact Me
       </Link>
     </div>
   );
