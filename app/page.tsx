b840a3319453a32b5fd894f23bbe07b3cb638434
Dominick Smith <dominick.smith938@gmail.com>
2025-02-27 13:32:34 -0600

    tailwind config update, new motion presets added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <h1 className="text-3xl text-center">Hi, I’m Dominick 👋</h1>
+      <div className="text-3xl text-center">
+        <h1 className="text-3xl text-center intro">Hi, I’m Dominick</h1>
+        <p className="handEmoji">👋</p>
+      </div>
       <Image
         src={`/pfp.jpg`}
         alt="A photo of me, Dominick"
-        width={500}
+        width={400}
         height={1}
         className="pfp flex justify-center mx-auto py-8"
       ></Image>
-      <h2 className="text-2xl text-center">
+      <h2 className="text-2xl text-center caption">
         I Build Modern & Scalable Web Experiences
       </h2>
     </main>
