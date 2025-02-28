50b00cab7d03378741717b17f658dcc29e66e406
Dominick Smith <dominick.smith938@gmail.com>
2025-02-27 18:59:24 -0600

    added prog langs svgs

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+
 export default function aboutMe() {
-  return <p className="text-center text-2xl"></p>;
+  return (
+    <>
+      <div className="text-center">
+        <p className="text-sm">Hello World!</p>
+      </div>
+      <div className="flex justify-evenly">
+        <div>
+          <p>1</p>
+        </div>
+        <div>
+          <p>2</p>
+        </div>
+        <div>
+          <p>3</p>
+        </div>
+      </div>
+    </>
+  );
 }
