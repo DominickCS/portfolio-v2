6b25dfddff97424aaf631addcbee9e3fa48f010a
Dominick Smith <dominick.smith938@gmail.com>
2025-03-06 11:45:52 -0600

    completed projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,10 +10,14 @@ export default function Projects() {
           You can find all of my projects on my GitHub profile!
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-6 text-center p-6">
-        <Link href={"https://github.com/DominickCS/pyZombie"}>
-          <div className="outline rounded-md p-4">
-            <h3 className="text-2xl py-2 underline">PyZombie</h3>
+      <div className="grid grid-cols-2 gap-4 text-center p-6">
+        <Link
+          href={"https://github.com/DominickCS/pyZombie"}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <div className="outline rounded-md p-4 hover:motion-preset-pulse-sm">
+            <h3 className="text-xl py-2 underline">PyZombie</h3>
             <p className="text-sm py-2">
               A top-down shooter game where the zombies don't stop! You are
               destined to die, but how long can you last?
@@ -23,17 +27,21 @@ export default function Projects() {
             </p>
           </div>
         </Link>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
+        <Link
+          href={"https://github.com/DominickCS/respotdl"}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <div className="outline rounded-md p-4 hover:motion-preset-pulse-sm">
+            <h3 className="text-xl py-2 underline">respotdl</h3>
+            <p className="text-sm py-2">
+              A Python script to download Music to a remote server.
+            </p>
+            <p className="text-xs text-blue-500 py-2">
+              #Python #SysAdmin #Utility
+            </p>
+          </div>
+        </Link>
       </div>
       <div className="flex justify-center py-6">
         <Button asChild className="p-8 text-xl">
