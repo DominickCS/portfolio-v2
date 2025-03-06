b1b7dadde231197689ccf65c8e90a99e5f63888a
Dominick Smith <dominick.smith938@gmail.com>
2025-03-06 11:26:48 -0600

    formatted projects page, template created

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,8 +10,19 @@ export default function Projects() {
           You can find all of my projects on my GitHub profile!
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-4 text-center py-6">
-        <p>TEST</p>
+      <div className="grid grid-cols-4 gap-6 text-center p-6">
+        <Link href={"https://github.com/DominickCS/pyZombie"}>
+          <div className="outline rounded-md p-4">
+            <h3 className="text-2xl py-2 underline">PyZombie</h3>
+            <p className="text-sm py-2">
+              A top-down shooter game where the zombies don't stop! You are
+              destined to die, but how long can you last?
+            </p>
+            <p className="text-xs text-blue-500 py-2">
+              #Python #PyGame #GameDev
+            </p>
+          </div>
+        </Link>
         <p>TEST</p>
         <p>TEST</p>
         <p>TEST</p>
